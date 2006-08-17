200040178458f1ee4bc07db711600b364b1bba74
Andreas Langer <a.langer@q-dsl.de>
2006-08-17 16:57:44 +0200

    add visualation server ( early early alpha )

--- recon.c
+++ recon.c
@@ -16,8 +16,11 @@
 
 #define MAXCHAR 4096
 #define PORT 1967
+#define S3D_PORT 1968
 #define ADDR_STR_LEN 16
 #define PACKET_FIELDS 5
+
+static int on = 1;
 
 void *node_server (void *srv_addr)
 {
@@ -25,7 +28,7 @@ void *node_server (void *srv_addr)
 	char addr_str[ADDR_STR_LEN];
 	
 	struct sockaddr_in server, client;
-	int sock, n, on=1,packet_count,i;
+	int sock, n, packet_count,i;
 	socklen_t len;
 	
 	sock = socket(PF_INET, SOCK_DGRAM,0 );
@@ -84,18 +87,70 @@ int main(int argc, char *argv[])
 {
 	pthread_t thread_node_server;
 	pthread_t thread_node_cleaner;
+	
+	struct sockaddr_in adr_serv, adr_client;
+	int n, len_inet, serv_socket, clnt_socket;
+	char buffer[MAXCHAR];
 
 	if(argc < 2)
 	{
 		fprintf(stderr,"Usage: recon <ip>\n");
 		return(EXIT_FAILURE);
 	}
-	
+
 	pthread_create(&thread_node_server, NULL, &node_server, argv[1]);
 	pthread_create(&thread_node_cleaner, NULL, &node_cleaner, NULL);
 	fprintf( stderr, "main: thread-id: %u\n",(unsigned int) pthread_self());
 	fprintf( stderr, "main: run node_server thread-id: %u\n", (unsigned int)thread_node_server);
 	fprintf( stderr, "main: run node_cleaner thread-id: %u\n", (unsigned int)thread_node_cleaner);
-	while(1) {}
+	
+	serv_socket = socket(PF_INET, SOCK_STREAM, 0);
+	memset(&adr_serv, 0, sizeof(adr_serv));
+	adr_serv.sin_family = AF_INET;
+	adr_serv.sin_port = htons(S3D_PORT);
+	inet_pton(AF_INET, argv[1],&adr_serv.sin_addr);
+
+	if(adr_serv.sin_addr.s_addr == INADDR_NONE)
+	{
+		printf("invalid adress %s", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+
+	if(serv_socket < 0)
+	{
+		printf("Cannot create socket => %s\n",strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+	
+	if(setsockopt(serv_socket, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(int)) < 0)
+	{
+		printf("Cannot enable ip: %s\n", strerror(errno));
+		close(serv_socket);
+		exit(EXIT_FAILURE);			
+	}
+	
+	if(bind( serv_socket, (struct sockaddr*)&adr_serv, sizeof(adr_serv)) < 0)
+	{
+		printf("Error by bind => %s\n",strerror(errno));
+		close(serv_socket);
+		exit(EXIT_FAILURE);
+	}
+	
+	if(listen(serv_socket,15) < 0)
+	{
+		printf("Error by listen => %s\n",strerror(errno));
+		close(serv_socket);
+		exit(EXIT_FAILURE);
+	}
+	printf("...accept connections on port %d\n", ntohs(adr_serv.sin_port));
+	while(1)
+	{
+		len_inet = sizeof(adr_client);
+		if((clnt_socket = accept(serv_socket, (struct sockaddr*)&adr_client,&len_inet)) == -1)
+		{
+			fprintf(stderr,"accept-error\n");
+		}
+	}
 	return EXIT_SUCCESS;
 }
+
